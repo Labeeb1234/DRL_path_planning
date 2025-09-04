150ee44af1b4618ff350d2ea2ac30e8524f44cff
Labeeb Nassar <labeebranassar@gmail.com>
2025-09-04 13:31:51 +0530

    updated 9-4-25

--- sb3_play.py
+++ sb3_play.py
@@ -5,13 +5,13 @@ from stable_baselines3.common.env_util import make_vec_env
 from gymnasium.wrappers import FlattenObservation
 
 
-model = PPO.load("logs/sb3/OmniBot-v0/2025-08-31_23-40-39/model_116000_steps.zip")
+model = PPO.load("logs/sb3/OmniBot-v0/2025-09-01_00-08-19/model_104000_steps.zip")
 env = make_vec_env('OmniBot-v0', n_envs=1, wrapper_class=FlattenObservation,  env_kwargs={"render_mode": "human"})
 obs = env.reset()
 done = False
 while not done:
-    for _ in range(1000):
-        action, _states = model.predict(obs, deterministic=True)
+    for _ in range(10000):
+        action, _states = model.predict(obs, deterministic=False)
         obs, rewards, terminated, info = env.step(action)
         done = terminated 
         env.render()
